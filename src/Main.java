6e60a05b14eba1d39ad9d54efd65a4816cb7f842
Tim Mikeladze <tim.mikeladze@gmail.com>
2013-11-13 22:42:40 -0800

    Added callbacks to change scene

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,12 @@
  * November 11, 2013
  */
 
-import view.LoginPane;
-import view.MainPane;
-import view.RegisterPane;
 import javafx.application.Application;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import view.Callbacks;
+import view.LoginPane;
 
 /**
  * Begins the program by instantiating and starting the JavaFX GUI.
@@ -19,36 +19,53 @@ import javafx.stage.Stage;
  * @author Mohammad Juma
  * @version 11-11-2013
  */
-public class Main extends Application {
-    
-    /**
-     * Private constructor, to prevent instantiation of this class.
-     *
-    private Main() {
-        // Does nothing.
-    }
-    */
-    
-    /**
-     * Used primarily within the IDE to launch the JavaFX GUI.
-     * Note: This method is not required for JavaFX applications as the JavaFX Packager
-     *       Tool embeds the JavaFX Launcher in the final JAR file.
-     * @param args Command line arguments.
-     */
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
-    /**
-     * @param primaryStage The main stage that holds this applications GUI.
-     */
-    @Override
-    public void start(Stage primaryStage) {
-        primaryStage.setTitle("TCSS360 Project");
+public class Main extends Application implements Callbacks {
 
-        Scene scene = new Scene(new LoginPane(), 950, 600);
-        primaryStage.setScene(scene);
-        
-        primaryStage.show();
-    }
+	/**
+	 * Private constructor, to prevent instantiation of this class.
+	 * 
+	 * private Main() { // Does nothing. }
+	 */
+
+	private Scene scene;
+	private Stage primaryStage;
+
+	/**
+	 * Used primarily within the IDE to launch the JavaFX GUI. Note: This method
+	 * is not required for JavaFX applications as the JavaFX Packager Tool
+	 * embeds the JavaFX Launcher in the final JAR file.
+	 * 
+	 * @param args
+	 *            Command line arguments.
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	/**
+	 * @param primaryStage
+	 *            The main stage that holds this applications GUI.
+	 */
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+
+		primaryStage.setTitle("TCSS360 Project");
+
+		LoginPane loginPane = new LoginPane();
+		loginPane.addCallbacks(this);
+		scene = new Scene(loginPane, 950, 600);
+		primaryStage.setScene(scene);
+
+		primaryStage.show();
+	}
+
+	@Override
+	public void changeScene(Parent parent) {
+		scene = new Scene(parent, 950, 600);
+		primaryStage.setScene(scene);
+
+		primaryStage.show();
+
+	}
 }
--- src/view/LoginPane.java
+++ src/view/LoginPane.java
@@ -10,8 +10,6 @@ package view;
 
 import java.sql.SQLException;
 
-import model.login.Login;
-import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -26,6 +24,7 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
+import model.login.Login;
 
 /**
  * JavaFX pane for displaying the login window.
@@ -35,114 +34,122 @@ import javafx.scene.text.Text;
  */
 public class LoginPane extends GridPane implements EventHandler {
 
-    /**
-     * Text component for showing the welcome text.
-     */
-    private Text welcomeText;
-    
-    /**
-     * Label for showing the email label text.
-     */
-    private Label emailLabel;
-    
-    /** 
-     * TextField for inputing the email.
-     */
-    private TextField emailTextField;
-    
-    /**
-     * Label for showing the password label text.
-     */
-    private Label passwordLabel;
-    
-    /**
-     * PasswordField for inputing the password.
-     */
-    private PasswordField passwordField;
-    
-    /**
-     * Button for signing into the application.
-     */
-    private Button signInButton;
-    
-    /** 
-     * Button for registering for an account.
-     */
-    private Button registerButton;
-    
-    /**
-     * Text for displaying sign in errors.
-     */
-    private Text signInText;
-    
-    /**
-     * Constructs a new LoginPane pane that extends GridPane and displays a prompt
-     * for the user to login or register.
-     */
-    public LoginPane() {
-        this.setAlignment(Pos.CENTER);
-        this.setHgap(10);
-        this.setVgap(10);
-        this.setPadding(new Insets(25, 25, 25, 25));
-        
-        create();
-    }
-    
-    /**
-     * Creates the main components of the LoginPane pane.
-     */
-    private void create() {
-        welcomeText = new Text("Welcome");
-        welcomeText.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
-        this.add(welcomeText, 0, 0, 2, 1);
+	private Callbacks callbacks;
 
-        emailLabel = new Label("Email:");
-        this.add(emailLabel, 0, 1);
+	/**
+	 * Text component for showing the welcome text.
+	 */
+	private Text welcomeText;
 
-        emailTextField = new TextField();
-        this.add(emailTextField, 1, 1);
+	/**
+	 * Label for showing the email label text.
+	 */
+	private Label emailLabel;
 
-        passwordLabel = new Label("Password:");
-        this.add(passwordLabel, 0, 2);
+	/**
+	 * TextField for inputing the email.
+	 */
+	private TextField emailTextField;
 
-        passwordField = new PasswordField();
-        this.add(passwordField, 1, 2);
-        
-        signInButton = new Button("Sign in");
-        signInButton.setOnAction(this);
-        
-        registerButton = new Button("Register");
-        registerButton.setOnAction(this);
-        
-        final HBox buttonHBox = new HBox(10);
-        buttonHBox.setAlignment(Pos.BOTTOM_RIGHT);
-        buttonHBox.getChildren().add(signInButton);
-        buttonHBox.getChildren().add(registerButton);
-        this.add(buttonHBox, 1, 4);
-        
-        signInText = new Text();
-        this.add(signInText, 1, 6);
-    }
-    
-    /**
-     * 
-     * @param event
-     */
-    @Override
-    public void handle(Event event) {
-            if (event.getSource() == signInButton) {
-                try {
-                    Login.loginUser(signInButton.getText());
-                } catch (SQLException exception) {
-                    //exception.printStackTrace();
-                    signInText.setFill(Color.FIREBRICK);
-                    signInText.setText("Sign in button pressed");
-                }
-            }
-            
-            if (event.getSource() == registerButton) {
-                
-            }
+	/**
+	 * Label for showing the password label text.
+	 */
+	private Label passwordLabel;
 
-    }
+	/**
+	 * PasswordField for inputing the password.
+	 */
+	private PasswordField passwordField;
+
+	/**
+	 * Button for signing into the application.
+	 */
+	private Button signInButton;
+
+	/**
+	 * Button for registering for an account.
+	 */
+	private Button registerButton;
+
+	/**
+	 * Text for displaying sign in errors.
+	 */
+	private Text signInText;
+
+	/**
+	 * Constructs a new LoginPane pane that extends GridPane and displays a
+	 * prompt for the user to login or register.
+	 */
+	public LoginPane() {
+		this.setAlignment(Pos.CENTER);
+		this.setHgap(10);
+		this.setVgap(10);
+		this.setPadding(new Insets(25, 25, 25, 25));
+
+		create();
+	}
+
+	public void addCallbacks(Callbacks callbacks) {
+		this.callbacks = callbacks;
+	}
+
+	/**
+	 * Creates the main components of the LoginPane pane.
+	 */
+	private void create() {
+		welcomeText = new Text("Welcome");
+		welcomeText.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+		this.add(welcomeText, 0, 0, 2, 1);
+
+		emailLabel = new Label("Email:");
+		this.add(emailLabel, 0, 1);
+
+		emailTextField = new TextField();
+		this.add(emailTextField, 1, 1);
+
+		passwordLabel = new Label("Password:");
+		this.add(passwordLabel, 0, 2);
+
+		passwordField = new PasswordField();
+		this.add(passwordField, 1, 2);
+
+		signInButton = new Button("Sign in");
+		signInButton.setOnAction(this);
+
+		registerButton = new Button("Register");
+		registerButton.setOnAction(this);
+
+		final HBox buttonHBox = new HBox(10);
+		buttonHBox.setAlignment(Pos.BOTTOM_RIGHT);
+		buttonHBox.getChildren().add(signInButton);
+		buttonHBox.getChildren().add(registerButton);
+		this.add(buttonHBox, 1, 4);
+
+		signInText = new Text();
+		this.add(signInText, 1, 6);
+	}
+
+	/**
+	 * 
+	 * @param event
+	 */
+	@Override
+	public void handle(Event event) {
+		if (event.getSource() == signInButton) {
+			try {
+				Login.loginUser(emailTextField.getText().trim());
+				if (callbacks != null) {
+					callbacks.changeScene(new MainPane());
+				}
+			} catch (SQLException e) {
+				signInText.setFill(Color.FIREBRICK);
+				signInText.setText(e.getMessage());
+			}
+		}
+
+		if (event.getSource() == registerButton) {
+
+		}
+
+	}
 }
