daae89ad21594583da1f85573cf5cc9d7e355e86
srdjans <srdjan206@hotmail.com>
2013-11-13 22:34:13 -0800

    Added test package and LoginTest

---
+++ src/test/LoginTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import model.login.Login;
+
+
+public class LoginTest {
+
+	@Test
+	public void testIsRegistered() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRegisterUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoginUser() {
+		fail("Not yet implemented");
+	}
+
+}
