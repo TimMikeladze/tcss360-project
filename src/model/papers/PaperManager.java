481512887a3601302a2e36b4a52c4ec5ad0140a9
Tim Mikeladze <tim.mikeladze@gmail.com>
2013-11-13 20:24:16 -0800

    removed abstract user and added empty paper manager

---
+++ src/model/papers/PaperManager.java
@@ -0,0 +1,14 @@
+package model.papers;
+
+
+public class PaperManager {
+	/*
+	 * public static int submitPaper(int conferenceID, int authorID, String
+	 * title, String description, File file) {
+	 * 
+	 * int id = Database.getInstance().createQuery("INSERT INTO papers
+	 * (ConferenceID, AuthorID, Title, Description ) return authorID;
+	 * 
+	 * }
+	 */
+}
