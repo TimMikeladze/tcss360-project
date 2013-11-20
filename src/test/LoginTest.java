93b95e3e5db6323c28100da257b9444313082023
srdjans <srdjan206@hotmail.com>
2013-11-19 16:10:27 -0800

    Added more tests for Login class

--- src/test/LoginTest.java
+++ src/test/LoginTest.java
@@ -4,10 +4,12 @@ import static org.junit.Assert.*;
 
 import java.sql.SQLException;
 
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import model.database.Database;
+import model.database.DatabaseException;
 import model.login.Login;
 
 
@@ -16,36 +18,53 @@ public class LoginTest {
 	/**
 	 * User's first name for testing.
 	 */
-	private String firstName;
+	private static String firstName1;
 	
 	/**
 	 * User's last name for testing.
 	 */
-	private String lastName;
+	private static String lastName1;
 	
 	/**
 	 * User's email for testing.
 	 */
-	private String email;
+	private static String email1;
+	
+	/**
+	 * Second user's first name for testing.
+	 */
+	private static String firstName2;
+	
+	/**
+	 * Second user's last name for testing.
+	 */
+	private static String lastName2;
+	
+	/**
+	 * Second user's email for testing.
+	 */
+	private static String email2;
 	
 	
 	/**
 	 * Initializes the tests.
 	 */
-	@Before
-	public void initialize() {
-		firstName = "Jon";
-		lastName = "Snow";
-		email = "youknownothingjonsnow@gmail.com";
+	@BeforeClass
+	public static void testSetup() {
+		firstName1 = "Jon";
+		lastName1 = "Snow";
+		email1 = "youknownothingjonsnow@gmail.com";
+		
+		firstName2 = "King";
+		lastName2 = "North";
+		email2 = "kingofthenorth@gmail.com";
 		
 		// Add the user 'Jon Snow' to the database
-		// Need to remove from database
 		Database.getInstance()
-		.createQuery(
-				"INSERT INTO users (Firstname, Lastname, Email) VALUES (:firstName, :lastName, :email)")
-		.addParameter("firstName", firstName)
-		.addParameter("lastName", lastName)
-		.addParameter("email", email).executeUpdate();
+		.createQuery("INSERT INTO users (Firstname, Lastname, Email) VALUES (:firstName, :lastName, :email)")
+		.addParameter("firstName", firstName1)
+		.addParameter("lastName", lastName1)
+		.addParameter("email", email1).executeUpdate();
 	}
 	
 	/*
@@ -58,7 +77,7 @@ public class LoginTest {
 		
 		
 		// Tests the case where the user should be in the database
-		assertTrue("User is in the database test for isRegistered()", Login.isRegistered(email));
+		assertTrue("User is in the database test for isRegistered()", Login.isRegistered(email1));
 	}
 
 	/**
@@ -68,7 +87,9 @@ public class LoginTest {
 	@Test
 	public void testRegisterUserException() {
 		try {
-			Login.registerUser(firstName, lastName, email);
+			// This user was already added to the database during setup so we should expect
+			// an exception
+			Login.registerUser(firstName1, lastName1, email1);
 			fail("Should have thrown an SQLException");
 		} catch (SQLException e) {
 			assertTrue(true);
@@ -80,7 +101,20 @@ public class LoginTest {
 	 */
 	@Test
 	public void testRegisterUser() {
-		fail("Not yet implemented");
+		try {
+			Login.registerUser(firstName2, lastName2, email2);
+		} catch (DatabaseException e) {
+			fail("registerUser threw an exception when it shouldn't have");
+			e.printStackTrace();
+		}
+		
+		assertTrue(Database.hasResults(Database
+				.getInstance()
+				.createQuery(
+						"SELECT 1 FROM users WHERE email = :email")
+				.addParameter("email", email2)
+				.executeAndFetchTable()));
+		
 	}
 
 	/**
@@ -89,6 +123,19 @@ public class LoginTest {
 	@Test
 	public void testLoginUser() {
 		fail("Not yet implemented");
+	}
+	
+	/**
+	 * Cleans up temporary data that was used in the tests.
+	 */
+	@AfterClass
+	public static void testCleanup() {
+		// Remove Jon Snow from database
+		Database.getInstance().createQuery("DELETE FROM users WHERE Email = :email")
+			.addParameter("email", email1).executeUpdate();
+		// Remove King North from database
+		Database.getInstance().createQuery("DELETE FROM users WHERE Email = :email")
+			.addParameter("email", email2).executeUpdate();
 	}
 
 }
