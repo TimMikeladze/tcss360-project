5ea052208494b3e12332691712ea82bed7d7463f
Tim Mikeladze <tim.mikeladze@gmail.com>
2013-11-26 18:48:17 -0800

    get all users

--- src/tests/Tester.java
+++ src/tests/Tester.java
@@ -4,19 +4,16 @@ package tests;
 import java.io.IOException;
 
 import model.database.DatabaseException;
-import model.login.Login;
 
 //TODO delete class when project is finished
 
 /**
  * This is just a class to test stuff
- * 
- * 
+ *
+ *
  */
 public class Tester {
-    
+
     public static void main(final String[] args) throws DatabaseException, InterruptedException, IOException {
-        //PaperManager.submitPaper(153, 125, "Royal Findings", "A study into the Queen's jewels", new File("tests/paper.txt"));
-        System.out.println(Login.loginUser("a@a.com").toString());
     }
 }
--- src/view/home/HomePane.java
+++ src/view/home/HomePane.java
@@ -36,78 +36,78 @@ import controller.user.LoggedUser;
  * @version 11-11-2013
  */
 public class HomePane extends GenericPane<GridPane> implements EventHandler {
-    
+
     /**
      * TODO COMMENT THIS
      */
     private static final int DOUBLE_CLICK = 2;
-    
+
     /**
      * TODO COMMENT THIS
      */
     private CustomTable<ConferenceRow> conferencesTable;
-    
+
     /**
      * TODO COMMENT THIS
      */
     private CustomTable<PaperRow> papersTable;
-    
+
     /**
      * TODO COMMENT THIS
      */
     private CustomTable<ReviewRow> reviewsTable;
-    
+
     /**
      * Column names of conferences in TableView.
      */
     private String[] conferencesColumnNames = { "Conference Name", "Program Chair", "Authors",
             "Reviewers", "Date" };
-    
+
     /**
      * The variable names for the conferences table used by Java FX's table classes
      */
     private String[] conferencesVariableNames = { "name", "programChair", "authors",
             "reviewers", "date" };
-    
+
     /**
      * Column names of papers in TableView.
      */
     private String[] papersColumnNames = { "Paper Name", "Conference Name", "Reviewed",
             "Revised", "Submission Date" };
-    
+
     /**
      * The variable names for the papers table used by Java FX's table classes
      */
     private String[] papersVariableNames = { "paperName", "conferenceName", "reviewed",
             "revised", "date" };
-    
+
     /**
      * Column names of reviews in TableView.
      */
     private String[] reviewsColumnsNames = { "Paper Name", "Author", "Conference Name",
             "Reviewed", "Submission Date" };
-    
+
     /**
      * The variable names for the reviews table used by Java FX's table classes
      */
     private String[] reviewsVariableNames = { "paperName", "author", "conferenceName",
             "reviewed", "date" };
-    
+
     /**
      * The list of conferences for the table.
      */
     private List<Conference> listOfConferences;
-    
+
     /**
      * The list of papers for the table.
      */
     private List<Paper> listOfPapers;
-    
+
     /**
      * The list of reviews for the table.
      */
     private List<Review> listOfReviews;
-    
+
     /**
      * Constructs a new HomePane pane that extends GridPane and displays the initial user
      * interface the user is greeted with upon login in.
@@ -117,45 +117,45 @@ public class HomePane extends GenericPane<GridPane> implements EventHandler {
         super(new GridPane());
         addMainPaneCallBacks(mainPaneCallbacks);
         addProgressSpinnerCallBacks(progressSpinnerCallbacks);
-        
+
         conferencesTable = new CustomTable<ConferenceRow>(conferencesColumnNames,
                 conferencesVariableNames);
         papersTable = new CustomTable<PaperRow>(papersColumnNames, papersVariableNames);
         reviewsTable = new CustomTable<ReviewRow>(reviewsColumnsNames, reviewsVariableNames);
-        
+
         pane.setAlignment(Pos.TOP_LEFT);
         pane.setHgap(10);
         pane.setVgap(10);
         pane.setPadding(new Insets(0, 5, 5, 5));
-        
+
         create();
     }
-    
+
     /**
      * Creates the main components of the HomePane pane.
      */
     private void create() {
         new LoadDataService(progressSpinnerCallbacks).start();
-        
+
         Text myConferencesText = new Text("My Conferences");
         myConferencesText.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
         conferencesTable.setOnMouseClicked(this);
         pane.add(myConferencesText, 0, 0);
         pane.add(conferencesTable, 0, 1);
-        
+
         Text myPapersText = new Text("My Papers");
         myPapersText.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
         papersTable.setOnMouseClicked(this);
         pane.add(myPapersText, 0, 3);
         pane.add(papersTable, 0, 4);
-        
+
         Text myReviewsText = new Text("My Reviews");
         myReviewsText.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
         papersTable.setOnMouseClicked(this);
         pane.add(myReviewsText, 0, 6);
         pane.add(reviewsTable, 0, 7);
     }
-    
+
     /**
      * Populates the tables from the database.
      */
@@ -179,7 +179,7 @@ public class HomePane extends GenericPane<GridPane> implements EventHandler {
             }
         }
     }
-    
+
     /**
      * TODO NEEDS COMMENTS
      */
@@ -191,6 +191,7 @@ public class HomePane extends GenericPane<GridPane> implements EventHandler {
                 //TODO replace with entering conference
                 int conferenceID = conferencesTable.getSelectionModel().getSelectedItem()
                         .getID();
+                //TODO fix this
                 mainPaneCallbacks.pushPane(new ConferencePane(Conference
                         .conferenceFromID(conferenceID), mainPaneCallbacks,
                         progressSpinnerCallbacks));
@@ -210,23 +211,23 @@ public class HomePane extends GenericPane<GridPane> implements EventHandler {
             }
         }
     }
-    
+
     /**
      * Loads conference, paper, and review data from database.
      */
     private class LoadDataService extends ProgressSpinnerService {
-        
+
         public LoadDataService(final ProgressSpinnerCallbacks progressSpinnerCallbacks) {
             super(progressSpinnerCallbacks);
         }
-        
+
         /**
          * Creates a new task for loading conferences
          */
         @Override
         protected Task<String> createTask() {
             return new Task<String>() {
-                
+
                 /**
                  * Calls the new task.
                  */
@@ -236,7 +237,7 @@ public class HomePane extends GenericPane<GridPane> implements EventHandler {
                         int id = LoggedUser.getInstance().getUser().getID();
                         listOfConferences = ConferenceManager.getConferencesForUser(id);
                         listOfPapers = PaperManager.getAuthorsSubmittedPapers(id);
-                        
+
                         //TODO implement reviews
                         //reviews = ReviewManager.getReviews(;
                         setSuccess(true);
@@ -248,7 +249,7 @@ public class HomePane extends GenericPane<GridPane> implements EventHandler {
                 }
             };
         }
-        
+
         /**
          * Called when a conference loading is done to populate table
          */
@@ -260,5 +261,5 @@ public class HomePane extends GenericPane<GridPane> implements EventHandler {
             super.succeeded();
         }
     }
-    
+
 }
