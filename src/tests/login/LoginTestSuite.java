604c7d60df9345cd7b0e60ae396ee94aa0750ce3
Jordan Matthews <jordan.matthews@wolfgis.com>
2013-12-05 11:42:24 -0800

    Fixed the LoginTest to get 100% then added a suite for the package

---
+++ src/tests/login/LoginTestSuite.java
@@ -0,0 +1,17 @@
+package tests.login;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+   LoginTest.class,
+})
+/**
+ * Suite of Tests for the model.Database Package
+ * 
+ * @author Jordan Matthews
+ * @version December 4, 2013
+ */
+public class LoginTestSuite {
+}
