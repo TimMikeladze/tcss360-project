ba12ee4329f1a6b477e034dbbc18784c2b273f74
Jordan Matthews <jordan.matthews@wolfgis.com>
2013-12-05 23:03:55 -0800

    Users test classes, 100% coverage for package

---
+++ src/tests/users/UserManagerTest.java
@@ -0,0 +1,76 @@
+package tests.users;
+
+import static org.junit.Assert.*;
+
+import model.database.Database;
+import model.users.UserManager;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests the Conference Manager Class from tests.conferences
+ * 
+ * @author Jordan Matthews
+ * @version December 5, 2013
+ */
+public class UserManagerTest {
+    /**
+     * User's first name for testing.
+     */
+    private String firstName;
+    /**
+     * User's last name for testing.
+     */
+    private String lastName;
+    /**
+     * User's email for testing.
+     */
+    private String email;
+    /**
+     * The first user ID
+     */
+    private int userID;
+    
+    /**
+     * Sets up the class for testing
+     */
+    @Before
+    public void setUp() {
+        // add user
+        firstName = "Jon";
+        lastName = "Snow";
+        email = "youknownothingjonsnow@gmail.com";
+
+        // Add the user 'Jon Snow' to the database
+        userID = Database.getInstance()
+                .createQuery("INSERT INTO users (Firstname, Lastname, Email) VALUES (:firstName, :lastName, :email)")
+                .addParameter("firstName", firstName)
+                .addParameter("lastName", lastName)
+                .addParameter("email", email)
+                .executeUpdate()
+                .getKey(Integer.class); 
+        UserManager temp = new UserManager();
+        temp.hashCode();
+    }
+    
+    /**
+     * Tests the getUsers method
+     */
+    @Test
+    public void testgetUsers() {
+        assertTrue("Users are there", UserManager.getUsers() != null);
+    }
+    
+    /**
+     * Cleans up the class after testing
+     */
+    @After
+    public void takeDown() {
+        Database.getInstance()
+            .createQuery("DELETE FROM users WHERE ID = :userID")
+            .addParameter("userID", userID)
+            .executeUpdate();
+    }
+}
---
+++ src/tests/util/FileHandlerTest.java
@@ -0,0 +1,20 @@
+package tests.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Tests the FileHandler class from model.util
+ * 
+ * @author Jordan Matthews
+ * @version December 5, 2013
+ */
+public class FileHandlerTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
