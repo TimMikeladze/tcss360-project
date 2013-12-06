26f1712d1c81933d9f91342cf397898178bcf674
Jordan Matthews <jordan.matthews@wolfgis.com>
2013-12-06 00:45:20 -0800

    More tests

---
+++ src/tests/recommendations/RecommendationTest.java
@@ -0,0 +1,25 @@
+package tests.recommendations;
+
+import static org.junit.Assert.*;
+
+import model.recommendations.Recommendation;
+
+import org.junit.Test;
+
+/**
+ * Tests the Recommendation class from model.recommendations
+ * 
+ * @author Jordan Matthews
+ * @version December 6, 2013
+ */
+public class RecommendationTest {
+    
+    /**
+     * Tests the constructor
+     */
+    @Test
+    public void testCons() {
+        assertTrue("Constructor", new Recommendation() != null);
+    }
+
+}
