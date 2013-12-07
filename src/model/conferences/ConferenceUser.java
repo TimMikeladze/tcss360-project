b4149c29ff14dab1b072eb7c36dbdc5c10f81d96
Tim Mikeladze <tim.mikeladze@gmail.com>
2013-12-06 18:10:51 -0800

    fixed

--- src/model/conferences/ConferenceUser.java
+++ src/model/conferences/ConferenceUser.java
@@ -17,7 +17,7 @@ public class ConferenceUser {
     /**
      * The id of the Conference.
      */
-    private int id;
+    private int conferenceID;
     
     /**
      * The Conference Users id.
@@ -52,21 +52,24 @@ public class ConferenceUser {
     /**
      * Create a Conference User object given the Conference id and user id.
      * 
-     * <dt><b>Precondition:</b><dd> requires conferenceID > 0 and is valid. <br>
-     *                              requires userID > 0 and is valid.
-     * <dt><b>Postcondition:</b><dd> ensures A ConferenceUser object is returned.
+     * <dt><b>Precondition:</b>
+     * <dd>requires conferenceID > 0 and is valid. <br>
+     * requires userID > 0 and is valid.
+     * <dt><b>Postcondition:</b>
+     * <dd>ensures A ConferenceUser object is returned.
+     * 
      * @param conferenceID The conferences id
      * @param userID The users id
      * @return the ConferenceUser object if found, else returns null
      */
     public static ConferenceUser userFromID(final int conferenceID, final int userID) {
         ConferenceUser user = null;
-        List<ConferenceUser> results = Database
-                .getInstance()
-                .createQuery(
-                        "SELECT cu.ConferenceID, cu.UserID, CONCAT(u.Firstname, ' ', u.Lastname) AS Username, u.Firstname, u.Lastname, cu.PermissionID, (SELECT COUNT(1) FROM assigned_papers AS a WHERE a.UserID = cu.UserID AND PermissionID = 300) AS AssignedAsSubProgramChair FROM conference_users AS cu JOIN users AS u ON u.ID = cu.UserID WHERE cu.ConferenceID = :conferenceID AND cu.UserID = :userID ORDER BY cu.PermissionID DESC LIMIT 1")
-                .addParameter("conferenceID", conferenceID).addParameter("userID", userID)
-                .executeAndFetch(ConferenceUser.class);
+        List<ConferenceUser> results = Database.getInstance()
+                                               .createQuery(
+                                                       "SELECT cu.ConferenceID, cu.UserID, CONCAT(u.Firstname, ' ', u.Lastname) AS Username, u.Firstname, u.Lastname, cu.PermissionID, (SELECT COUNT(1) FROM assigned_papers AS a WHERE a.UserID = cu.UserID AND PermissionID = 300) AS AssignedAsSubProgramChair FROM conference_users AS cu JOIN users AS u ON u.ID = cu.UserID WHERE cu.ConferenceID = :conferenceID AND cu.UserID = :userID ORDER BY cu.PermissionID DESC LIMIT 1")
+                                               .addParameter("conferenceID", conferenceID)
+                                               .addParameter("userID", userID)
+                                               .executeAndFetch(ConferenceUser.class);
         if (Database.hasResults(results)) {
             user = results.get(0);
         }
@@ -76,19 +79,25 @@ public class ConferenceUser {
     /**
      * Gets the Conferences id.
      * 
-     * <dt><b>Precondition:</b><dd> none
-     * <dt><b>Postcondition:</b><dd> ensures The Conferences id is returned.
+     * <dt><b>Precondition:</b>
+     * <dd>none
+     * <dt><b>Postcondition:</b>
+     * <dd>ensures The Conferences id is returned.
+     * 
      * @return he Conferences id
      */
     public int getConferenceID() {
-        return id;
+        return conferenceID;
     }
     
     /**
      * Gets the Conference Users id.
      * 
-     * <dt><b>Precondition:</b><dd> none
-     * <dt><b>Postcondition:</b><dd> ensures The Conferences Users id is returned.
+     * <dt><b>Precondition:</b>
+     * <dd>none
+     * <dt><b>Postcondition:</b>
+     * <dd>ensures The Conferences Users id is returned.
+     * 
      * @return the Conference Users id
      */
     public int getUserID() {
@@ -98,8 +107,11 @@ public class ConferenceUser {
     /**
      * Gets the Conference Users role.
      * 
-     * <dt><b>Precondition:</b><dd> none
-     * <dt><b>Postcondition:</b><dd> ensures The Conferences Users role is returned.
+     * <dt><b>Precondition:</b>
+     * <dd>none
+     * <dt><b>Postcondition:</b>
+     * <dd>ensures The Conferences Users role is returned.
+     * 
      * @return the Conference Users role
      */
     public String getRole() {
@@ -116,8 +128,11 @@ public class ConferenceUser {
     /**
      * Gets the Conference Users full name.
      * 
-     * <dt><b>Precondition:</b><dd> none
-     * <dt><b>Postcondition:</b><dd> ensures The Conferences Users full name is returned.
+     * <dt><b>Precondition:</b>
+     * <dd>none
+     * <dt><b>Postcondition:</b>
+     * <dd>ensures The Conferences Users full name is returned.
+     * 
      * @return the Conference Users full name.
      */
     public String getUsername() {
@@ -127,8 +142,11 @@ public class ConferenceUser {
     /**
      * Gets the Conference Users first name.
      * 
-     * <dt><b>Precondition:</b><dd> none
-     * <dt><b>Postcondition:</b><dd> ensures The Conferences Users first name is returned.
+     * <dt><b>Precondition:</b>
+     * <dd>none
+     * <dt><b>Postcondition:</b>
+     * <dd>ensures The Conferences Users first name is returned.
+     * 
      * @return the Conference Users first name
      */
     public String getFirstname() {
@@ -138,8 +156,11 @@ public class ConferenceUser {
     /**
      * Gets the Conference Users last name.
      * 
-     * <dt><b>Precondition:</b><dd> none
-     * <dt><b>Postcondition:</b><dd> ensures The Conferences Users last name is returned.
+     * <dt><b>Precondition:</b>
+     * <dd>none
+     * <dt><b>Postcondition:</b>
+     * <dd>ensures The Conferences Users last name is returned.
+     * 
      * @return the Conference Users last name.
      */
     public String getLastname() {
@@ -149,8 +170,12 @@ public class ConferenceUser {
     /**
      * Gets the number of Papers the Conference User is assigned to as a Subprogram Chair.
      * 
-     * <dt><b>Precondition:</b><dd> none
-     * <dt><b>Postcondition:</b><dd> ensures The number of Papers the Conference User is assigned to as a Subprogram Chair is returned.
+     * <dt><b>Precondition:</b>
+     * <dd>none
+     * <dt><b>Postcondition:</b>
+     * <dd>ensures The number of Papers the Conference User is assigned to as a Subprogram Chair
+     * is returned.
+     * 
      * @return the number of Papers the Conference User is assigned to as a Subprogram Chair
      */
     public int getAssignedAsSubProgramChair() {
