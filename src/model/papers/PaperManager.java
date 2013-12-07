fbf2218ced5b3c83558f757574351bf2e34d4119
M Juma <github@mhjuma.com>
2013-12-06 21:43:01 -0800

    bleh

--- src/model/papers/PaperManager.java
+++ src/model/papers/PaperManager.java
@@ -46,12 +46,12 @@ public class PaperManager {
      * @throws IOException Signals that an I/O exception has occurred.
      */
     @Permission(level = 100)
-    public static void submitPaper(final int conferenceID, final int authorID, final String title, final String description, final File file)
+    public static void submitPaper(final int conferenceID, final int authorID,
+            final String title, final String description, final File file)
             throws DatabaseException, IOException {
         if (Database.hasResults(Database.getInstance()
-                                        .createQuery("SELECT 1 FROM conferences WHERE NOW() > Date AND ID = :id")
-                                        .addParameter("id", conferenceID)
-                                        .executeAndFetchTable())) {
+                .createQuery("SELECT 1 FROM conferences WHERE NOW() > Date AND ID = :id")
+                .addParameter("id", conferenceID).executeAndFetchTable())) {
             throw new DatabaseException(DatabaseErrors.PAST_CONFERENCE_DATE);
         }
         
@@ -60,13 +60,11 @@ public class PaperManager {
                     .createQuery(
                             "INSERT INTO papers (ConferenceID, AuthorID, Title, Description, SubmissionDate, File, FileExtension) VALUES (:conferenceID, :authorID, :title, :description, NOW(), :file, :fileExtension)")
                     .addParameter("conferenceID", conferenceID)
-                    .addParameter("authorID", authorID)
-                    .addParameter("title", title)
+                    .addParameter("authorID", authorID).addParameter("title", title)
                     .addParameter("description", description)
                     .addParameter("file", FileHandler.convertFileToBytes(file))
                     .addParameter("fileExtension", FileHandler.getFileExtension(file))
-                    .executeUpdate()
-                    .getKey(Integer.class);
+                    .executeUpdate().getKey(Integer.class);
             Database.getInstance()
                     .createQuery(
                             "INSERT IGNORE INTO conference_users (ConferenceID, UserID, PermissionID) VALUES (:conferenceID, :userID, :permissionID)")
@@ -89,8 +87,7 @@ public class PaperManager {
     public static void removePaper(final int paperID, final int authorID) {
         Database.getInstance()
                 .createQuery("DELETE FROM papers WHERE ID = :paperID AND AuthorID = :authorID")
-                .addParameter("paperID", paperID)
-                .addParameter("authorID", authorID)
+                .addParameter("paperID", paperID).addParameter("authorID", authorID)
                 .executeUpdate();
     }
     
@@ -102,7 +99,8 @@ public class PaperManager {
      * @return list of papers
      */
     @Permission(level = 300)
-    public static List<Paper> getAssignedPapersForSubprogramChair(final int conferenceID, final int userID) {
+    public static List<Paper> getAssignedPapersForSubprogramChair(final int conferenceID,
+            final int userID) {
         return getAssignedPapers(conferenceID, userID, PermissionLevel.SUBPROGRAM_CHAIR);
     }
     
@@ -115,7 +113,8 @@ public class PaperManager {
      * @return The list of papers
      */
     @Permission(level = 200)
-    public static List<Paper> getAssignedPapersForReviewer(final int conferenceID, final int userID) {
+    public static List<Paper> getAssignedPapersForReviewer(final int conferenceID,
+            final int userID) {
         return getAssignedPapers(conferenceID, userID, PermissionLevel.REVIEWER);
     }
     
@@ -127,31 +126,33 @@ public class PaperManager {
      * @param permission the permission
      * @return the assigned papers
      */
-    private static List<Paper> getAssignedPapers(final int conferenceID, final int userID, final PermissionLevel permission) {
-        return Database.getInstance()
-                       .createQuery(
-                               "SELECT p.ConferenceID, p.ID AS PaperID, p.Title, p.Description, p.AuthorID, p.SubmissionDate, p.Status, p.Revised, p.FileExtension, CONVERT(p.File USING utf8) AS File, p.RevisionDate, Recommended, CONCAT(u.Firstname, ' ', u.Lastname) AS Username FROM papers AS p JOIN assigned_papers AS a ON a.PaperID = p.ID JOIN users AS u ON u.ID = p.AuthorID WHERE p.ConferenceID = :conferenceID AND a.UserID = :userID AND a.PermissionID = :permissionID")
-                       .addParameter("conferenceID", conferenceID)
-                       .addParameter("userID", userID)
-                       .addParameter("permissionID", permission.getPermission())
-                       .executeAndFetch(Paper.class);
+    private static List<Paper> getAssignedPapers(final int conferenceID, final int userID,
+            final PermissionLevel permission) {
+        return Database
+                .getInstance()
+                .createQuery(
+                        "SELECT p.ConferenceID, p.ID AS PaperID, p.Title, p.Description, p.AuthorID, p.SubmissionDate, p.Status, p.Revised, p.FileExtension, CONVERT(p.File USING utf8) AS File, p.RevisionDate, Recommended, CONCAT(u.Firstname, ' ', u.Lastname) AS Username FROM papers AS p JOIN assigned_papers AS a ON a.PaperID = p.ID JOIN users AS u ON u.ID = p.AuthorID WHERE p.ConferenceID = :conferenceID AND a.UserID = :userID AND a.PermissionID = :permissionID")
+                .addParameter("conferenceID", conferenceID).addParameter("userID", userID)
+                .addParameter("permissionID", permission.getPermission())
+                .executeAndFetch(Paper.class);
     }
     
     public static List<Paper> getAssignedPapersForUser(final int userID) {
-        return Database.getInstance()
-                       .createQuery(
-                               "SELECT p.ConferenceID, p.ID AS PaperID, p.Title, p.Description, p.AuthorID, p.SubmissionDate, p.Status, p.Revised, p.FileExtension, CONVERT(p.File USING utf8) AS File, p.RevisionDate, Recommended, CONCAT(u.Firstname, ' ', u.Lastname) AS Username FROM papers AS p JOIN assigned_papers AS a ON a.PaperID = p.ID JOIN users AS u ON u.ID = p.AuthorID WHERE a.UserID = :userID GROUP BY p.ID")
-                       .addParameter("userID", userID)
-                       .executeAndFetch(Paper.class);
+        return Database
+                .getInstance()
+                .createQuery(
+                        "SELECT p.ConferenceID, p.ID AS PaperID, p.Title, p.Description, p.AuthorID, p.SubmissionDate, p.Status, p.Revised, p.FileExtension, CONVERT(p.File USING utf8) AS File, p.RevisionDate, Recommended, CONCAT(u.Firstname, ' ', u.Lastname) AS Username FROM papers AS p JOIN assigned_papers AS a ON a.PaperID = p.ID JOIN users AS u ON u.ID = p.AuthorID WHERE a.UserID = :userID GROUP BY p.ID")
+                .addParameter("userID", userID).executeAndFetch(Paper.class);
     }
     
-    public static List<Paper> getAssignedPapersForUserInConference(final int userID, final int conferenceID) {
-        return Database.getInstance()
-                       .createQuery(
-                               "SELECT p.ConferenceID, p.ID AS PaperID, p.Title, p.Description, p.AuthorID, p.SubmissionDate, p.Status, p.Revised, p.FileExtension, CONVERT(p.File USING utf8) AS File, p.RevisionDate, Recommended, CONCAT(u.Firstname, ' ', u.Lastname) AS Username FROM papers AS p JOIN assigned_papers AS a ON a.PaperID = p.ID JOIN users AS u ON u.ID = p.AuthorID WHERE a.UserID = :userID AND p.ConferenceID = :conferenceID GROUP BY p.ID")
-                       .addParameter("userID", userID)
-                       .addParameter("conferenceID", userID)
-                       .executeAndFetch(Paper.class);
+    public static List<Paper> getAssignedPapersForUserInConference(final int userID,
+            final int conferenceID) {
+        return Database
+                .getInstance()
+                .createQuery(
+                        "SELECT p.ConferenceID, p.ID AS PaperID, p.Title, p.Description, p.AuthorID, p.SubmissionDate, p.Status, p.Revised, p.FileExtension, CONVERT(p.File USING utf8) AS File, p.RevisionDate, Recommended, CONCAT(u.Firstname, ' ', u.Lastname) AS Username FROM papers AS p JOIN assigned_papers AS a ON a.PaperID = p.ID JOIN users AS u ON u.ID = p.AuthorID WHERE a.UserID = :userID AND p.ConferenceID = :conferenceID GROUP BY p.ID")
+                .addParameter("userID", userID).addParameter("conferenceID", userID)
+                .executeAndFetch(Paper.class);
     }
     
     /**
@@ -162,21 +163,15 @@ public class PaperManager {
      * @param permission the user's permission level
      * @throws DatabaseException
      */
-<<<<<<< HEAD
     @Permission(level = 100)
     public static void assignPaper(final int paperID, final int userID,
             final PermissionLevel permission) throws DatabaseException {
-=======
-    @Permission(level = 300)
-    public static void assignPaper(final int paperID, final int userID, final PermissionLevel permission) throws DatabaseException {
->>>>>>> 401bbde1bb39d24cf38aaf703e7351a87b3490dc
         if (getPaperAuthorID(paperID) != userID) {
             Database.getInstance()
-                    .createQuery("INSERT IGNORE INTO assigned_papers (PaperID, UserID, PermissionID) VALUES (:paperID, :userID, :permissionID)")
-                    .addParameter("paperID", paperID)
-                    .addParameter("userID", userID)
-                    .addParameter("permissionID", permission.getPermission())
-                    .executeUpdate();
+                    .createQuery(
+                            "INSERT IGNORE INTO assigned_papers (PaperID, UserID, PermissionID) VALUES (:paperID, :userID, :permissionID)")
+                    .addParameter("paperID", paperID).addParameter("userID", userID)
+                    .addParameter("permissionID", permission.getPermission()).executeUpdate();
         }
         else {
             throw new DatabaseException(DatabaseErrors.CANT_ASSIGN_PAPER);
@@ -192,13 +187,10 @@ public class PaperManager {
      */
     public static int getPaperAuthorID(final int paperID) throws DatabaseException {
         Table t = Database.getInstance()
-                          .createQuery("SELECT AuthorID FROM papers WHERE ID = :paperID")
-                          .addParameter("paperID", paperID)
-                          .executeAndFetchTable();
+                .createQuery("SELECT AuthorID FROM papers WHERE ID = :paperID")
+                .addParameter("paperID", paperID).executeAndFetchTable();
         if (Database.hasResults(t)) {
-            return t.rows()
-                    .get(0)
-                    .getInteger(0);
+            return t.rows().get(0).getInteger(0);
         }
         else {
             throw new DatabaseException(DatabaseErrors.PAPER_DOES_NOT_EXIST);
@@ -236,8 +228,7 @@ public class PaperManager {
         int paperStatus = status ? 2 : 1;
         Database.getInstance()
                 .createQuery("UPDATE papers SET Status = :paperStatus WHERE ID = :id")
-                .addParameter("paperStatus", paperStatus)
-                .addParameter("id", paperID)
+                .addParameter("paperStatus", paperStatus).addParameter("id", paperID)
                 .executeUpdate();
     }
     
@@ -249,31 +240,32 @@ public class PaperManager {
      */
     @Permission(level = 400)
     public static List<Paper> getPapers(final int conferenceID) {
-        return Database.getInstance()
-                       .createQuery(
-                               "SELECT p.ConferenceID, p.ID AS PaperID, p.Title, p.Description, p.AuthorID, p.SubmissionDate, p.Status, p.Revised, p.FileExtension, CONVERT(p.File USING utf8) AS File, p.RevisionDate, p.Recommended, CONCAT(u.Firstname, ' ', u.Lastname) AS Username FROM papers AS p JOIN users AS u ON u.ID = p.AuthorID WHERE p.ConferenceID = :conferenceID")
-                       .addParameter("conferenceID", conferenceID)
-                       .executeAndFetch(Paper.class);
+        return Database
+                .getInstance()
+                .createQuery(
+                        "SELECT p.ConferenceID, p.ID AS PaperID, p.Title, p.Description, p.AuthorID, p.SubmissionDate, p.Status, p.Revised, p.FileExtension, CONVERT(p.File USING utf8) AS File, p.RevisionDate, p.Recommended, CONCAT(u.Firstname, ' ', u.Lastname) AS Username FROM papers AS p JOIN users AS u ON u.ID = p.AuthorID WHERE p.ConferenceID = :conferenceID")
+                .addParameter("conferenceID", conferenceID).executeAndFetch(Paper.class);
     }
     
     @Permission(level = 100)
-    public static List<Paper> getAuthorsSubmittedPapers(final int authorID, final int conferenceID) {
-        return Database.getInstance()
-                       .createQuery(
-                               "SELECT p.ConferenceID, p.ID AS PaperID, p.Title, p.Description, p.AuthorID, p.SubmissionDate, p.Status, p.Revised, p.FileExtension, CONVERT(p.File USING utf8) AS File, p.RevisionDate, p.Recommended, CONCAT(u.Firstname, ' ', u.Lastname) AS Username FROM papers AS p JOIN users AS u ON u.ID = p.AuthorID WHERE p.AuthorID = :authorID AND p.ConferenceID = :conferenceID")
-                       .addParameter("authorID", authorID)
-                       .addParameter("conferenceID", conferenceID)
-                       .executeAndFetch(Paper.class);
+    public static List<Paper> getAuthorsSubmittedPapers(final int authorID,
+            final int conferenceID) {
+        return Database
+                .getInstance()
+                .createQuery(
+                        "SELECT p.ConferenceID, p.ID AS PaperID, p.Title, p.Description, p.AuthorID, p.SubmissionDate, p.Status, p.Revised, p.FileExtension, CONVERT(p.File USING utf8) AS File, p.RevisionDate, p.Recommended, CONCAT(u.Firstname, ' ', u.Lastname) AS Username FROM papers AS p JOIN users AS u ON u.ID = p.AuthorID WHERE p.AuthorID = :authorID AND p.ConferenceID = :conferenceID")
+                .addParameter("authorID", authorID).addParameter("conferenceID", conferenceID)
+                .executeAndFetch(Paper.class);
         
     }
     
     @Permission(level = 100)
     public static List<Paper> getAuthorsSubmittedPapers(final int authorID) {
-        return Database.getInstance()
-                       .createQuery(
-                               "SELECT p.ConferenceID, p.ID AS PaperID, p.Title, p.Description, p.AuthorID, p.SubmissionDate, p.Status, p.Revised, p.FileExtension, CONVERT(p.File USING utf8) AS File, p.RevisionDate, p.Recommended, CONCAT(u.Firstname, ' ', u.Lastname) AS Username FROM papers AS p JOIN users AS u ON u.ID = p.AuthorID WHERE p.AuthorID = :authorID")
-                       .addParameter("authorID", authorID)
-                       .executeAndFetch(Paper.class);
+        return Database
+                .getInstance()
+                .createQuery(
+                        "SELECT p.ConferenceID, p.ID AS PaperID, p.Title, p.Description, p.AuthorID, p.SubmissionDate, p.Status, p.Revised, p.FileExtension, CONVERT(p.File USING utf8) AS File, p.RevisionDate, p.Recommended, CONCAT(u.Firstname, ' ', u.Lastname) AS Username FROM papers AS p JOIN users AS u ON u.ID = p.AuthorID WHERE p.AuthorID = :authorID")
+                .addParameter("authorID", authorID).executeAndFetch(Paper.class);
         
     }
     
@@ -285,50 +277,47 @@ public class PaperManager {
      * @return number of papers author has submitted
      */
     public static int getNumberOfSubmittedPapers(final int conferenceID, final int authorID) {
-        return Database.getInstance()
-                       .createQuery("SELECT COUNT(1) FROM papers WHERE ConferenceID = :conferenceID AND AuthorID = :authorID")
-                       .addParameter("conferenceID", conferenceID)
-                       .addParameter("authorID", authorID)
-                       .executeAndFetchTable()
-                       .rows()
-                       .get(0)
-                       .getInteger(0);
+        return Database
+                .getInstance()
+                .createQuery(
+                        "SELECT COUNT(1) FROM papers WHERE ConferenceID = :conferenceID AND AuthorID = :authorID")
+                .addParameter("conferenceID", conferenceID).addParameter("authorID", authorID)
+                .executeAndFetchTable().rows().get(0).getInteger(0);
     }
     
     public static List<ConferenceUser> getAssignedUsers(final int paperID) {
-        return Database.getInstance()
-                       .createQuery(
-                               "SELECT cu.ConferenceID, cu.UserID, CONCAT(u.Firstname, ' ', u.Lastname) AS Username, cu.PermissionID FROM conference_users AS cu JOIN users AS u ON u.ID = cu.UserID JOIN papers AS p ON p.ConferenceID = cu.ConferenceID WHERE p.ID = :paperID ORDER BY cu.PermissionID DESC")
-                       .addParameter("paperID", paperID)
-                       .executeAndFetch(ConferenceUser.class);
+        return Database
+                .getInstance()
+                .createQuery(
+                        "SELECT cu.ConferenceID, cu.UserID, CONCAT(u.Firstname, ' ', u.Lastname) AS Username, cu.PermissionID FROM conference_users AS cu JOIN users AS u ON u.ID = cu.UserID JOIN papers AS p ON p.ConferenceID = cu.ConferenceID WHERE p.ID = :paperID ORDER BY cu.PermissionID DESC")
+                .addParameter("paperID", paperID).executeAndFetch(ConferenceUser.class);
     }
     
     @Permission(level = 300)
     public static void recommendPaper(final int paperID) {
         Database.getInstance()
                 .createQuery("UPDATE papers SET Recommended = 1 WHERE ID = :paperID")
-                .addParameter("paperID", paperID)
-                .executeUpdate();
+                .addParameter("paperID", paperID).executeUpdate();
     }
     
     @Permission(level = 100, strict = true)
     public static void reuploadPaper(final int paperID, final File file) throws IOException {
         Database.getInstance()
-                .createQuery("UPDATE papers SET File = :file, FileExtension = :fileExtension WHERE ID = :paperID")
+                .createQuery(
+                        "UPDATE papers SET File = :file, FileExtension = :fileExtension WHERE ID = :paperID")
                 .addParameter("file", FileHandler.convertFileToBytes(file))
                 .addParameter("fileExtension", FileHandler.getFileExtension(file))
                 .executeUpdate();
     }
     
     public static String getSubProgramChair(final int paperID) {
-        List<ConferenceUser> result = Database.getInstance()
-                                              .createQuery(
-                                                      "SELECT CONCAT(u.Firstname, ' ', u.Lastname) AS Username FROM assigned_papers AS a JOIN users AS u ON a.UserID = u.ID WHERE a.PermissionID = 300 AND a.PaperID = :paperID")
-                                              .addParameter("paperID", paperID)
-                                              .executeAndFetch(ConferenceUser.class);
+        List<ConferenceUser> result = Database
+                .getInstance()
+                .createQuery(
+                        "SELECT CONCAT(u.Firstname, ' ', u.Lastname) AS Username FROM assigned_papers AS a JOIN users AS u ON a.UserID = u.ID WHERE a.PermissionID = 300 AND a.PaperID = :paperID")
+                .addParameter("paperID", paperID).executeAndFetch(ConferenceUser.class);
         if (result != null && result.size() > 0) {
-            return result.get(0)
-                         .getUsername();
+            return result.get(0).getUsername();
         }
         return "";
         
